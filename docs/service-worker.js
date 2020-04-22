429c1fc446548cd2589c086c696703806273f1dc
adharamonzon <adharamonzon@gmail.com>
2020-04-22 20:31:56 +0200

    Update docs

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.f504855f9ffecaa530c65696fa20a835.js"
+  "./precache-manifest.088e276e0d70e4cd62702246593cce39.js"
 );
 
 self.addEventListener('message', (event) => {
